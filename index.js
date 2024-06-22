a45d2774d85cae0776650cdc2b4859e59276fd37
void-the-droid <bhuwan626282@gmail.com>
2024-06-22 16:21:26 +0545

    getting news functions add

---
+++ index.js
@@ -0,0 +1,15 @@
+//Getting the news for the page
+
+// async  function getNews(){
+//     try{
+//     const news = await fetch("https://newsapi.org/v2/everything?q=weather&from=2024-06-21&sortBy=popularity&apiKey=cca101db7fdf4a5e844f3f740bd75c3e",{mode:'cors',mehod:"GET"})
+//     const newsInfo =await news.json()
+//     console.log(newsInfo.articles[0])
+// }
+// catch(error){
+//     console.log("Can't fetch the data")
+// }
+
+
+// }
+// getNews()
